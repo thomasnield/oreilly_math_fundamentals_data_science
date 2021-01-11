efdf6a0ac6f880182cf6fbdfa0b32838c7897404
Thomas Nield <thomasnield@live.com>
2021-01-11 07:32:42 -0600

    initial commit

--- linear_regression/05_multiple_linear_regression_sklearn.py
+++ linear_regression/05_multiple_linear_regression_sklearn.py
@@ -18,5 +18,5 @@ print("Coefficients = {0}".format(fit.coef_))
 print("Intercept = {0}".format(fit.intercept_))
 print("z = {0} + {1}x + {2}y".format(fit.intercept_, fit.coef_[0], fit.coef_[1]))
 
-# Predict a new “z" value for x = 3.5
+# Predict a new “z" value for x = 3.5 and y = 4.5
 print("x = 3.5, y = 4.5, z = {0}".format(fit.predict([[3.5, 4.5]])))
