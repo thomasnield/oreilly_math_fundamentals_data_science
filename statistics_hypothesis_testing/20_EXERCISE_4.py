79b458b1d9ef5dd787fe110774a0d91a8571f32e
Thomas Nield <thomasnield@live.com>
2021-01-25 09:07:52 -0600

    Update 20_EXERCISE_4.py

--- statistics_hypothesis_testing/20_EXERCISE_4.py
+++ statistics_hypothesis_testing/20_EXERCISE_4.py
@@ -1,11 +1,11 @@
 from scipy.stats import norm
 
-# $10,345 in sales on average, $552 standard deviation
-mean = 10345
+# $11,641 in sales on average, $552 standard deviation
+mean = 11641
 std_dev = 552
 
-# Probability of $11,641 or more
-p1 = 1.0 - norm.cdf(11641, mean, std_dev)
+# Probability of $10,345 or less
+p1 = norm.cdf(10345, mean, std_dev)
 
 # Take advantage of symmetry for other side
 p2 = p1
@@ -13,4 +13,4 @@ p2 = p1
 # P-value of both tails
 p_value = p1 + p2
 
-print(p_value) # 0.01888333596496139
+print(p_value)  # 0.01888333596496139
