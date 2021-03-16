194c6c5ac6250def1fd214ddcbd094011f0c74ec
Thomas Nield <thomasnield@live.com>
2021-03-16 10:29:29 -0500

    Update 03_median.py

--- statistics_hypothesis_testing/03_median.py
+++ statistics_hypothesis_testing/03_median.py
@@ -1,15 +1,15 @@
 # Number of pets each person owns
-sample = [1, 3, 2, 5, 7, 0, 2, 3]
+sample = [0, 1, 5, 7, 9, 10, 14,15]
 
 def median(values):
     ordered = sorted(values)
     print(ordered)
     n = len(ordered)
-    mid = int(n / 2) - 1
+    mid = int(n / 2) - 1 if n % 2 == 0 else int(n/2)
 
     if n % 2 == 0:
         return (ordered[mid] + ordered[mid+1]) / 2.0
     else:
         return ordered[mid]
 
-print(median(sample)) # prints 2.5
+print(median(sample)) # prints 5
