3fa62df0a28a1c70e116c1ca41017e0840ea7472
Thomas Nield <thomasnield@live.com>
2021-03-22 07:28:43 -0500

    Update 06_standard_deviation.py

--- statistics_hypothesis_testing/06_standard_deviation.py
+++ statistics_hypothesis_testing/06_standard_deviation.py
@@ -5,7 +5,7 @@ sample = [1, 3, 2, 5, 7, 0, 2, 3]
 
 def variance(values):
     mean = sum(values) / len(values)
-    var = sum((v - mean) ** 2 for v in values) / len(values)
+    var = sum((v - mean) ** 2 for v in values) / (len(values) - 1)
     return var
 
 def std_dev(values):
