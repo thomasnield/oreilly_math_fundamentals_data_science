f44b65e8028fa5b89c35add9479a6b3cebc2cacf
Thomas Nield <thomasnield@live.com>
2021-04-14 16:52:59 -0500

    Update 06_train_test_split.py

--- linear_regression/06_train_test_split.py
+++ linear_regression/06_train_test_split.py
@@ -1,3 +1,6 @@
+# RESOURCE:
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
