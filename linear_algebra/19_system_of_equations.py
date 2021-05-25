cd0ff45e78d7e397e9d57c2dbf171f5a5725e600
Thomas Nield <thomasnield@live.com>
2021-05-25 11:52:22 -0500

    Update 19_system_of_equations.py

--- linear_algebra/19_system_of_equations.py
+++ linear_algebra/19_system_of_equations.py
@@ -1,22 +1,16 @@
-from numpy import array
-from numpy.linalg import inv
+from sympy import *
 
 # 4x + 2y + 4z = 44
 # 5x + 3y + 7z = 56
 # 9x + 3y + 6z = 72
 
-A = array([
+A = Matrix([
     [4, 2, 4],
     [5, 3, 7],
     [9, 3, 6]
 ])
 
-B = array([
-    44,
-    56,
-    72
-])
+# dot product between A and its inverse
+identity = A.inv() * A
 
-X = inv(A).dot(B)
-
-print(X)
+print(identity)
