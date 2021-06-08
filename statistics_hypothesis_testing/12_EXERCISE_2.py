0e789322ed70937965a3a4bc618e9a55c21628b5
Thomas Nield <thomasnield@live.com>
2021-06-08 08:04:27 -0500

    Update 12_EXERCISE_2.py

--- statistics_hypothesis_testing/12_EXERCISE_2.py
+++ statistics_hypothesis_testing/12_EXERCISE_2.py
@@ -1,8 +1,25 @@
+from post_answer import submit
+
+"""
+A market researcher estimates that the Z-Phone 
+smart phone has a mean consumer life of 42 months 
+with a standard deviation of 8 months. 
+
+Assuming a normal distribution, what is the probability 
+a given random Z-Phone will last between 20 and 30 months? 
+
+Correct the code below to print the answer. 
+
+Then execute the script by typing this command in the terminal:
+python3 task2.py 
+"""
+
 from scipy.stats import norm
 
 mean = 42
 std_dev = 8
 
-x = norm.cdf(30, mean, std_dev) - norm.cdf(20, mean, std_dev)
+x = norm.cdf(30, mean, std_dev) 
 
-print(x)
+# Submits the answer, please don't modify! 
+submit(2, x, 4)
