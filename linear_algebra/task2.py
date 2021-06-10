14bd45b7c344ce8fe8fd9a41b6966ac9d64bd2f1
Thomas Nield <thomasnield@live.com>
2021-06-10 16:05:49 -0500

    Create task2.py

---
+++ linear_algebra/task2.py
@@ -0,0 +1,24 @@
+"""
+Scale and add these two vectors. 
+
+Vector A is [2,1] and needs to be scaled by a factor of 3. 
+
+Vector B is [1,3] and needs to be scaled by a factor of 2. 
+
+What is the resulting vector when you scale and add these
+two vectors together? 
+
+Complete the code below, then execute the script by 
+typing this command in the terminal:
+python3 task2.py 
+"""
+
+from numpy import array
+
+a = array([3,2])
+b = array([2,-1])
+
+# scale and sum the vectors
+scaled_a_plus_scaled_b = ? 
+
+print(scaled_a_plus_scaled_b)
