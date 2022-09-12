43be0d7c3c0663f71858d241ded0aa1033ac88d6
Thomas Nield <thomasnield@live.com>
2022-09-12 07:48:53 -0500

    Update 10_EXERCISE_3.py

--- probability/10_EXERCISE_3.py
+++ probability/10_EXERCISE_3.py
@@ -1,8 +1,18 @@
+"""
+There is a 30% chance of rain today, and a 40% chance your umbrella 
+order will arrive on time. 
+
+However, you found out if it rains there is only 20% chance 
+your umbrella will arrive on time. 
+
+What is the probability it will rain and your umbrella will arrive on time? 
+
+Complete and run the Python script below to calculate your answer.
+
+Execute with the command: python3 task3.py 
+"""
 p_rain = .30
 p_umbrella_on_time = .40
 p_umbrella_on_time_given_rain = .20
 
-# Probability of it raining and umbrella arriving on time 
-p_rain_and_umbrella_on_time = p_rain * p_umbrella_on_time_given_rain
-
-print(p_rain_and_umbrella_on_time)
+p_rain_and_umbrella_on_time = ?
